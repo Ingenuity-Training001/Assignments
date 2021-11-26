8395c4afca3090c5184c33115ea786c48aa8bb34
Jay Upadhyay <92813379+JayUpadhyay007@users.noreply.github.com>
2021-11-26 15:48:16 +0530

    TS_pixi2

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,39 @@
-import { Application, ParticleContainer,Rectangle } from "pixi.js";
-import { Alien } from "./Alien";
+import {Application,SimplePlane} from "pixi.js";
+
 const app:Application = new Application({
-  width: 1280,
-  height: 720,
+    width:1280,
+    height:720,
 });
 document.body.appendChild(app.view);
-const sprites:ParticleContainer = new ParticleContainer(10000, {
-  scale: true,
-  position: true,
-  rotation: true,
-  uvs: true,
-  alpha: true,
-});
 
-app.stage.addChild(sprites);
-const aliens: Alien[] = [];
-
-const totalDudes:number = 20;
 app.loader
-  .add("bunny", "https://pixijs.io/examples/examples/assets/bunny.png")
-  .add("egghead", "https://pixijs.io/examples/examples/assets/eggHead.png")
-  .add(
-    "monsters",
-    "https://pixijs.io/examples/examples/assets/spritesheet/monsters.png"
-  )
-  .load((l, r) => {
-    for (let i = 0; i < totalDudes; i++) {
-      const dude:Alien = new Alien(r["monsters"].texture);
-      dude.anchor.set(0.5);
-      dude.scale.set(0.8 + Math.random() * 0.3);
+    .add('bg_grass', 'assets/img.jpg')
+    .load(build);
 
-      dude.x = Math.random() * app.screen.width;
-      dude.y = Math.random() * app.screen.height;
-
-      dude.tint = Math.random() * 0xffffff;
-
-      dude.direction = Math.random() * Math.PI * 2;
-      dude.turningSpeed = Math.random() - 0.8;
-      dude.speed = 2 + Math.random() * 2;
-
-      aliens.push(dude);
-      sprites.addChild(dude);
+    function build() {
+        // Create a new texture
+        const texture = app.loader.resources.bg_grass.texture;
+    
+        // Create the simple plane
+        const verticesX = 10;
+        const verticesY = 10;
+        const plane = new SimplePlane(texture, verticesX, verticesY);
+    
+        plane.x = 100;
+        plane.y = 100;
+    
+        app.stage.addChild(plane);
+    
+        // Get the buffer for vertice positions.
+        const buffer = plane.geometry.getBuffer('aVertexPosition');
+    
+        // Listen for animate update
+        app.ticker.add((delta) => {
+            // Randomize the vertice positions a bit to create movement.
+            for (let i = 0; i < buffer.data.length; i++) {
+                buffer.data[i] += (Math.random() - 0.5);
+            }
+            buffer.update();
+        });
     }
-    const dudeBoundsPadding = 100;
-
-    const dudeBounds = new Rectangle(
-      -dudeBoundsPadding,
-      -dudeBoundsPadding,
-      app.screen.width + dudeBoundsPadding * 2,
-      app.screen.height + dudeBoundsPadding * 2
-    );
-    let tick=0;
-    app.ticker.add(() => {
-      // iterate through the dudes and update their position
-      for (let i = 0; i < aliens.length; i++) {
-        const dude = aliens[i];
-        dude.updatePosition(tick);
-
-        // wrap the dudes by testing their bounds...
-        dude.checkCollisionBounds(dudeBounds);
-      }
-      tick+=0.1
-    });
-  });
+        
